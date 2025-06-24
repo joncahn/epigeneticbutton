4dac533a31d84031994d1c0518eb4cac5e430039
Jonathan Cahn <cahnjonathan@gmail.com>
2025-06-24 13:15:23 -0400

    Update R_call_DEGs.R

--- scripts/R_call_DEGs.R
+++ scripts/R_call_DEGs.R
@@ -143,7 +143,7 @@ stat_table<-group_by(allDEG, firstsample, secondsample, DEG) %>%
 	
 for (sample1 in genotypes) {
 	nunique<-filter(uniqueDEGs, Sample == sample1) %>%
-		mutate(secondsample="Unique")) %>%
+		mutate(secondsample="Unique") %>%
 		group_by(firstsample, secondsample, DEG) %>%
 		summarize(Nb=n())
 	
