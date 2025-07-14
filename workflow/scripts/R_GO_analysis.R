93754ddb51ece3ba974f0e7b398cacbff709bb19
Jonathan Cahn <cahnjonathan@gmail.com>
2025-07-14 14:48:56 -0400

    Update R_GO_analysis.R

--- workflow/scripts/R_GO_analysis.R
+++ workflow/scripts/R_GO_analysis.R
@@ -106,12 +106,12 @@ if (startsWith(backgroundfile, "results/RNA/DEG/counts__")) {
 			for ( ont in c("BP","MF") ) {
 				print(paste0("Getting ",ont," for ",samplename))
 				sampletable<-filter(target, Sample==samp, DEG==deg)
-				sampletable
+				print(sampletable)
 				if (nrow(sampletable) > 0) {
 					myInterestedGenes<-unique(unlist(sampletable$GID))
 					geneList<-factor(as.integer(allGenes %in% myInterestedGenes))
 					names(geneList)<-allGenes
-					geneList
+					print(geneList)
 					getGO(geneList, sampletable, ont, samplename)
 				}
 			}
