b86cd4a24fbb404d1d4b5b668b63e5f5568b0076
Jonathan Cahn <cahnjonathan@gmail.com>
2025-07-14 00:19:00 -0400

    Update R_build_GO_database.R

--- workflow/scripts/R_build_GO_database.R
+++ workflow/scripts/R_build_GO_database.R
@@ -32,8 +32,8 @@ fChr<-unique(select(genes, GID, Chr))
 
 makeOrgPackage(gene_info=fSym, chromosome=fChr, go=fGO,
               version="0.1",
-              maintainer="EPIC@epicbutton",
-              author="EPIC@epicbutton",
+              maintainer="user <user@epicbutton>",
+              author="user <user@epicbutton>",
               outputDir = paste0("./genomes/",refgenome,"/GO/"),
               tax_id = ncbiID,
               genus = genus,
