1d6e8c776e72c55c86971401f69e479d43a663cb
Jonathan Cahn <cahnjonathan@gmail.com>
2025-09-26 16:21:08 -0400

    Update R_browser_plot.R

--- workflow/scripts/R_browser_plot.R
+++ workflow/scripts/R_browser_plot.R
@@ -62,6 +62,9 @@ for ( i in c(1:tot) ) {
 
 axistrack<-GenomeAxisTrack(scale=0.1, labelPos="above")
 
+genes(genes)
+transcripts(genes)
+
 genetrack<-GeneRegionTrack(genes, name="Genes", shape="smallArrow", col="black", fill="grey60", rotation.title=0, cex.title=0.5, lwd=0.1, collapseTranscripts="meta", showId=TRUE, stacking = "dense",transcriptAnnotation="genes")
 
 tetrack<-AnnotationTrack(tes, name="TEs", stacking = "dense", fill = "lightgreen", shape="box", rotation.title=0, cex.title=0.5, lwd=0.1)
